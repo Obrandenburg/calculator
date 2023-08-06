a00ba0cbc69847b1e5f6858cd300a1c9261ebd0d
Obrandenburg <107490086+Obrandenburg@users.noreply.github.com>
2023-08-06 13:54:07 -0500

    Initial commit:
    Non-styled calculator with number and function buttons.
    Takes user input and pushes actual numbers to array for later evaluation.
    Changes the display to reflect current working number.

---
+++ script.js
@@ -0,0 +1,17 @@
+document.querySelector('.functions');
+
+operate = function(event, a, b){
+    if (target.value = '+'){
+        let result = a + b;
+    }
+    else if (target.value = '-'){
+        let result = a - b;
+    }
+    else if (target.value = '*'){
+        let result = a * b;
+    }
+    else if (target.value = '/'){
+        let result = a / b;
+    }
+    else{console.log('error')};
+}
